348620c5a3c89641941489fe9b9b11dc8f6d274d
Fabien LALLEMAND <fabien.lallemand@d2-si.eu>
2017-11-08 17:05:25 +0100

    add a second public subnet

--- coreInfra/main.tf
+++ coreInfra/main.tf
@@ -21,3 +21,15 @@ resource "aws_subnet" "pub1" {
     Name = "public_subnet_az1"
   }
 }
+
+resource "aws_subnet" "pub2" {
+  vpc_id            = "${aws_vpc.main.id}"
+  availability_zone = "eu-west-1b"
+
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags {
+    Name = "public_subnet_az2"
+  }
+}
