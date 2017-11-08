c642d71d48389f6dc020bb97f255dee5e281533a
Fabien LALLEMAND <fabien.lallemand@d2-si.eu>
2017-11-08 17:26:15 +0100

    add route table

--- coreInfra/main.tf
+++ coreInfra/main.tf
@@ -41,3 +41,16 @@ resource "aws_internet_gateway" "gw" {
     Name = "public Internet gateway"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "route table - lab 4 test"
+  }
+}
--- coreInfra/test-ec2.tf
+++ coreInfra/test-ec2.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "allow_ssh" {
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "-1"
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
